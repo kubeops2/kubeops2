0450ad9562303d495a4660d38507b7951cf4a013
glayneon <yeraongon@gmail.com>
2024-09-24 15:10:40 +0900

    add conditionals for root in init.sh file.

--- init.sh
+++ init.sh
@@ -6,25 +6,27 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+echo "이 것은 루트로 실행 됩니다."
+
 # /etc/security/limits.conf 파일 수정
-echo "시스템 전체의 파일 디스크립터 제한 설정 중..."
-if grep -q "nofile" /etc/security/limits.conf; then
-    sed -i '/nofile/d' /etc/security/limits.conf
-fi
-echo "*       soft    nofile  65535" >> /etc/security/limits.conf
-echo "*       hard    nofile  65535" >> /etc/security/limits.conf
-echo "완료: /etc/security/limits.conf 파일이 수정되었습니다."
+# echo "시스템 전체의 파일 디스크립터 제한 설정 중..."
+# if grep -q "nofile" /etc/security/limits.conf; then
+#     sed -i '/nofile/d' /etc/security/limits.conf
+# fi
+# echo "*       soft    nofile  65535" >> /etc/security/limits.conf
+# echo "*       hard    nofile  65535" >> /etc/security/limits.conf
+# echo "완료: /etc/security/limits.conf 파일이 수정되었습니다."
 
 # /etc/sysctl.conf 파일 수정
-echo "커널 파라미터 설정 중..."
-if grep -q "fs.inotify.max_user_watches" /etc/sysctl.conf; then
-    sed -i '/fs.inotify.max_user_watches/d' /etc/sysctl.conf
-fi
-if grep -q "fs.inotify.max_user_instances" /etc/sysctl.conf; then
-    sed -i '/fs.inotify.max_user_instances/d' /etc/sysctl.conf
-fi
+# echo "커널 파라미터 설정 중..."
+# if grep -q "fs.inotify.max_user_watches" /etc/sysctl.conf; then
+#     sed -i '/fs.inotify.max_user_watches/d' /etc/sysctl.conf
+# fi
+# if grep -q "fs.inotify.max_user_instances" /etc/sysctl.conf; then
+#     sed -i '/fs.inotify.max_user_instances/d' /etc/sysctl.conf
+# fi
 
 # swap off 처리
 # /etc/fstab 에 swap 라인 주석 처리 필요
-sed -i 's/\/swap/#swap/g' /etc/fstab
-sudo swapoff -a
+# sed -i 's/\/swap/#swap/g' /etc/fstab
+# sudo swapoff -a
