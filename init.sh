ce2855a97f379e7c619bcd15881b5c37736e0396
glayneon <yeraongon@gmail.com>
2024-09-25 16:15:11 +0900

    Update init.sh script file.

--- init.sh
+++ init.sh
@@ -32,12 +32,16 @@ fatal() {
 
 # 루트 권한 확인
 if [ "$EUID" -ne 0 ]
-  then info "이 스크립트는 sudo 권한으로 실행해야 합니다."
-  info "ex> $ curl -sSL url | sudo bash"
+  then info "::: 이 스크립트는 sudo 권한으로 실행해야 합니다."
+  info "::: ex> $ curl -sSL url | sudo bash"
   exit -1
 fi
 
-info "sudo 로 실행 가능 합니다."
+info "::: sudo 로 실행 가능 합니다."
+sleep(1)
+
+# line breaker
+echo;echo
 
 # Check variables
 # INSTALL_RKE2_VERSION 과 INSTALL_RKE2_TYPE 을 체크한다.
@@ -46,7 +50,7 @@ RKE2_TYPE='server'
 
 
 # /etc/security/limits.conf 파일 수정
-info "시스템 전체의 파일 디스크립터 제한 설정 중..."
+info "::: 시스템 전체의 파일 디스크립터 제한 설정 중..."
 if grep -q "nofile" /etc/security/limits.conf; then
     sed -i '/nofile/d' /etc/security/limits.conf
 fi
@@ -55,7 +59,7 @@ echo "*       hard    nofile  65535" >> /etc/security/limits.conf
 info "완료: /etc/security/limits.conf 파일이 수정되었습니다."
 
 # /etc/sysctl.conf 파일 수정
-info "Applying Kernel Parameters..."
+info "::: Applying Kernel Parameters..."
 if grep -q "fs.inotify.max_user_watches" /etc/sysctl.conf; then
     sed -i '/fs.inotify.max_user_watches/d' /etc/sysctl.conf
 fi
@@ -65,36 +69,46 @@ fi
 
 echo "fs.inotify.max_user_watches=524288" >> /etc/sysctl.conf
 echo "fs.inotify.max_user_instances=512" >> /etc/sysctl.conf
-info "완료: /etc/sysctl.conf 파일이 수정되었습니다."
+info "::: 완료: /etc/sysctl.conf 파일이 수정되었습니다."
 
 sysctl -p
 
 # swap off 처리
 # /etc/fstab 에 swap 라인 주석 처리 필요
-info "Deactivating Swap space..."
+info "::: Deactivating Swap space..."
 sed -i 's/\/swap/#swap/g' /etc/fstab
 swapoff -a
 
 # Install Tools
-info "Installing kubectl, helm, yq and jq ..."
+info "::: Installing kubectl, helm, yq and jq ..."
 apt update
 apt install iptables -y
 snap install yq jq
 # apt update
 
 # Install RKE2 As a server
-info "Install RKE2 with ${RKE2_VERSION} and ${RKE2_TYPE} mode ..."
+info "::: Install RKE2 with ${RKE2_VERSION} and ${RKE2_TYPE} mode ..."
+sleep(1)
+
+echo;echo
+
 curl -sfL https://get.rke2.io | INSTALL_RKE2_VERSION=${RKE2_VERSION} INSTSLL_RKE2_TYPE=${RKE2_TYPE} sh -
+
+info "::: Enabling and Starting RKE2-Server service ..."
+sleep(1)
 systemctl enable rke2-server.service
 systemctl start rke2-server.service
 # journalctl -u rke2-server -f
-info "Done!! -- Installing RKE2 with ${RKE2_VERSION} and ${RKE2_TYPE} mode ..."
+info "::: Done!!! - Enabling and Starting RKE2-Server service ... "
+sleep(1)
 
 # install helm
+info "::: Install Helm..."
 snap install helm --classic
 
 # Setting up User Environment
-info "Setting User ${USER} Environment..."
+info "::: Setting User ${USER} Environment..."
+sleep(1)
 mkdir -p ~/.kube
 cp /etc/rancher/rke2/rke2.yaml ~/.kube/config
 chown -R $USER:$USER ~/.kube
@@ -104,10 +118,14 @@ PATH=$PATH:/var/lib/rancher/rke2/bin
 echo "PATH=$PATH:/var/lib/rancher/rke2/bin" >> ~/.bashrc
 export PATH=$PATH:/var/lib/rancher/rke2/bin
 
-info "Testing kubectl for getting nodes..."
+info "::: Testing kubectl for getting nodes..."
 kubectl get nodes
+sleep(1)
 
+echo;echo
 # Setting up k9s
+
+info "::: Install k9s tool..."
 curl -sS https://webi.sh/k9s | sh
 source ~/.config/envman/PATH.env
 echo "source <(kubectl completion bash)" >> ~/.bashrc
